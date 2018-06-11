998843b5204224edfd9bb4bafd69230ac1fbdf11
Neha <nverma39@gmail.com>
2018-06-11 18:49:43 +0530

    Updated URLs for parse for RHPT

--- omod/src/main/webapp/resources/scripts/intelehealth_additional_docs_images/intelehealth_additional_docs_images.service.js
+++ omod/src/main/webapp/resources/scripts/intelehealth_additional_docs_images/intelehealth_additional_docs_images.service.js
@@ -2,7 +2,7 @@ intelehealthAdditionalDocs.factory('intelehealthAdditionalDocsFactory', [ '$http
 		function($http, $q) {
 			return {
 				fetchAdditionalDocuments : function(patientUuid, visitId) {
-					var url = "http://139.59.85.135:1337/parse/classes/AdditionalDocuments?where={\"PatientID\":\"" + patientUuid +"\",\"VisitID\":\"" + visitId + "\"}";
+					var url = "http://139.59.56.40:1337/parse/classes/AdditionalDocuments?where={\"PatientID\":\"" + patientUuid +"\",\"VisitID\":\"" + visitId + "\"}";
 					var headers = {headers:  {
 				        'X-Parse-Application-Id': 'app2',
 				        'X-Parse-REST-API-Key': 'undefined'
