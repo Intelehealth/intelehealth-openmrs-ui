e87d3c60d575ab2ff850703d3b97595779d2cc3c
Kundan Kumar Sinha <kundanmail55@gmail.com>
2018-11-04 20:59:11 +0530

    added observation in recentvisit fragment

--- omod/src/main/webapp/resources/scripts/recent_visits/recent_visits.controller.js
+++ omod/src/main/webapp/resources/scripts/recent_visits/recent_visits.controller.js
@@ -15,10 +15,11 @@ recentVisits.filter('vitalsDate', function() {
 		   return x.toFixed(1);
 	 };
  });
+
  
  recentVisits.controller('recentVisitController', function($scope, $http,
 		 $timeout, recentVisitFactory, $location) {
-	 $scope.observation = [];
+	 var observation = {}
 	 $scope.recentVisits = [];
 	 $scope.visitList = [];
 	 $scope.visitDetails = {};
@@ -29,9 +30,7 @@ recentVisits.filter('vitalsDate', function() {
 	 recentVisitFactory.fetchRecentVisits().then(
 			 function(data) {
 				 $scope.visitList = data.data.results;
-				//  console.log('hi',data.data.results)
 				 $scope.links = [];
-				 var k = 0;
 				 angular.forEach($scope.visitList, function(value, key) {
 					 if($scope.patientId === value.patient.uuid){
 						 var uuid = value.uuid;
@@ -39,32 +38,21 @@ recentVisits.filter('vitalsDate', function() {
 						 $scope.vitaluuid.push(value.uuid);
 						 recentVisitFactory.fetchVisitDetails(uuid).then(function(data) {
 							  $scope.visitDetails = data.data;
-							 	$scope.visitid = data.data.uuid;
-							 recentVisitFactory.fetchVisitEncounterObs($scope.visitid).then(function(data) {
-								 console.log(data.data.display)
-								 var str = data.data.display
-								 var pattern=/[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]/gi;
-								 var date = str.match(pattern)
-								 console.log(date)
-								$scope.visitDetails = data.data.encounters[1].obs;
-								angular.forEach($scope.visitDetails, (v) => {
+							  recentVisitFactory.fetchVisitEncounterObs(data.data.uuid).then(function(data) {
+									$scope.visitDetails = data.data.encounters[1].obs;
+									angular.forEach($scope.visitDetails, (v) => {
 									var display = v.display
 									if (display.match("CURRENT COMPLAINT") !== null) {
 									var obs = display.split('<b>');
-									// console.log(obs)
-									var l = 0
-									$scope.observation[k] = new Array(obs.length-1)
+									var b = " "
 									for (var i = 1; i<obs.length; i++) {
 										var obs1 = obs[i].split('<')
-										var a = obs1[0]
-										$scope.observation[k][l] = a 
-										l++;
+										var b = b + " | " + obs1[0]
+										value.observation = b
 									}
 								}
 								})	
-								k++
 							 })
-								value.obser= $scope.observation
 							 if ($scope.visitDetails.stopDatetime == null || $scope.visitDetails.stopDatetime == undefined) {
 								 value.visitStatus = "Active";
 							 }
@@ -138,44 +126,4 @@ recentVisits.filter('vitalsDate', function() {
 			 }, function(error) {
 				 console.log(error);
 			 });
-			 
-
-
-
-
-
-// $scope.objects = [];
-//     var url = "/" + OPENMRS_CONTEXT_PATH + "/ws/rest/v1/encounter";
-//         url += "?patient=" + $scope.patientId;
-//         url += "&encounterType=" + "8d5b27bc-c2cc-11de-8d13-0010c6dffd0f";
-//     $http.get(url)
-//     	  .then(function(response) {
-//         	$scope.vitalEncounters = response.data.results;
-// 		$scope.vitalEncountersUrl = [];
-// 		$scope.url2 = [];
-// 		angular.forEach($scope.vitalEncounters, function(value, key){
-// 			$scope.vitalEncountersUrl.push(value.uuid);
-// 		});
-// 		angular.forEach($scope.vitalEncountersUrl, function(value, key){
-//         		var url2 = "/" + OPENMRS_CONTEXT_PATH + "/ws/rest/v1/encounter/";
-// 	        	    url2 += value;
-//                 	$scope.url2.push(url2);
-// 		});
-// 		$scope.obs = $scope.url2.length;
-// 		angular.forEach($scope.url2, function(item){
-// 			$http.get(item)
-// 			      .then(function(response) {
-// 					  console.log(response.data.obs)
-// 		  		   $scope.recentVisits.push(response.data.);
-// 						 //\$scope.trustedHtml = \$sce.trustAsHtml(item);
-// 			      }, function(response) {
-// 	       			   $scope.error = "Get Encounter Observations Went Wrong";
-// 	       		           $scope.statuscode = response.status;
-// 			      });
-// 		});
-//           }, function(response) {
-// 		$scope.error = "Get Visit Encounters Went Wrong";
-//         	$scope.statuscode = response.status;
-//     	});
- console.log($scope.observation)
- });
+});
